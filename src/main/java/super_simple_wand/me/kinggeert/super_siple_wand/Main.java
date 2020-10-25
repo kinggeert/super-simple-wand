ef7dca0af40a286949a1b523a12aa6f5bdfbbe4e
kinggeert <geertbruls@outlook.com>
2020-10-25 12:02:47 +0100

    started project

---
+++ src/main/java/super_simple_wand/me/kinggeert/super_siple_wand/Main.java
@@ -0,0 +1,17 @@
+package super_simple_wand.me.kinggeert.super_siple_wand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
